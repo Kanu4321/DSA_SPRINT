1fcd4a6a1db23f50bfa9975b31649246374e6677
Abhinav <eveningstar1812@gmail.com>
2025-11-28 22:28:11 +0530

    commit

---
+++ increment.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int x;
+    cout << "ENter a no. :";
+    cin >> x;
+    cout << "x++ :" << x++ << endl;
+    cout << "++x :" << ++x << endl;
+    cout << "Final value :" << x;
+    return 0;
+}
